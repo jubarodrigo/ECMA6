d8e8a2609f0026489c4e412dc73d6cf6beabf075
Juba <rgjuba@gmail.com>
2019-02-02 15:01:54 -0200

    loading

--- src/main.js
+++ src/main.js
@@ -15,6 +15,18 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
+    setLoading(loading = true){
+        if(loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        }else{
+            document.getElementById('loading').remove();
+        }
+    }
+
     async addRepository(event) {
         event.preventDefault();
 
@@ -22,20 +34,29 @@ class App {
         if (repoInput.lenght === 0)
             return;
 
-        const response = await api.get(`/repos/${repoInput}`);
+        this.setLoading();
 
-        const {name, description, html_url, owner: {avatar_url}} = response.data;
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
 
-        this.repositories.push({
-            name,
-            description,
-            avatar_url,
-            html_url,
-        });
+            const {name, description, html_url, owner: {avatar_url}} = response.data;
+    
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+    
+            this.inputEl.value = '';
+    
+            this.render();            
+        } catch (error) {
+            alert('Erro ao buscar repositório');
+        }
 
-        this.inputEl.value = '';
+        this.setLoading(false);
 
-        this.render();
     }
 
     render() {
